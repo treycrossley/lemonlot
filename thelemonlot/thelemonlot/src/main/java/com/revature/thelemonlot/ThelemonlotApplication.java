9420166c6af2480dd2641fc8d21d195e21812b4d
Eric Tapia-Garcia <gtapia.eric@hotmail.com>
2024-08-08 15:06:36 -0600

    Spring Initializr code with no dependencies

---
+++ thelemonlot/thelemonlot/src/main/java/com/revature/thelemonlot/ThelemonlotApplication.java
@@ -0,0 +1,13 @@
+package com.revature.thelemonlot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThelemonlotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThelemonlotApplication.class, args);
+	}
+
+}
