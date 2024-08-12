64e23ffbcc3bb3468a748ae9255a89b51c6e34aa
ZBDGIT <zbd.github@gmail.com>
2024-08-12 14:01:42 -0700

    Minor Change

--- lemonlot-backend/src/main/java/com/revature/thelemonlot/repository/TransactionRepository.java
+++ lemonlot-backend/src/main/java/com/revature/thelemonlot/repository/TransactionRepository.java
@@ -4,6 +4,7 @@ package com.revature.thelemonlot.repository;
 import com.revature.thelemonlot.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
---
+++ lemonlot-backend/src/main/java/com/revature/thelemonlot/service/TransactionService.java
@@ -0,0 +1,4 @@
+package com.revature.thelemonlot.service;
+
+public class TransactionService {
+}
