3193664c1e9fafac564afe73ea99028a7c9dff26
Trey Crossley <treycrossley@gmail.com>
2024-08-11 21:48:48 -0700

    removed test comment

--- lemonlot-backend/src/main/java/com/revature/thelemonlot/ThelemonlotApplication.java
+++ lemonlot-backend/src/main/java/com/revature/thelemonlot/ThelemonlotApplication.java
@@ -3,8 +3,6 @@ package com.revature.thelemonlot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//Test Comment :)
-
 @SpringBootApplication
 public class ThelemonlotApplication {
 
